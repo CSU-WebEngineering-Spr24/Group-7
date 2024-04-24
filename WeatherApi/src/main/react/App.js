226459943701ccbb7ebf470dd897a39e59c2498e
somireddy11 <somireddy511@gmail.com>
2024-04-24 03:30:17 -0400

    react set up with web pack

--- WeatherApi/src/main/react/App.js
+++ WeatherApi/src/main/react/App.js
@@ -2,27 +2,16 @@
 
 import React, {Component} from "react";
 import './App.css'
-import NasaDataComponent from './components/NasaDataComponent'
 import { Link, Route, Routes } from 'react-router-dom'
 import  {BrowserRouter} from "react-router-dom"
+import Welcome from "./Welcome";
 //import WelcomeMessage from "./components/WelcomeMessage";
 function App() {
 
 
   return (
     <>
-    {<nav>
-        <a href="/">home</a>
-     <a href="/nasa"> Click here to retrieve NASA Data</a>
-     </nav> }
-
-  <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<h1>Nasa Home page</h1>}/>
-          <Route path='/nasa' element={<<h1>Nasa Home page</h1>/>} />
-        </Routes>
-    </BrowserRouter>
-      
+     <Welcome/>
      
     </>
   )
